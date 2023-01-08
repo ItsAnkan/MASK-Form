778eb6ae9099e651eb6363fe26393280efa574cf
ItsAnkan <ankansahadgp@gmail.com>
2023-01-08 13:59:03 +0530

    Testing from VS Code

--- app.js
+++ app.js
@@ -2,3 +2,10 @@ const express = require("express");
 const http = require("http");
 const path = require("path");
 const nunjucks = require("nunjucks");
+const app = require("../WebDev Task Backend/app");
+
+const env = nunjucks.configure(
+    {
+        express: app
+    }
+)
